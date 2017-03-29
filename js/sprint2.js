4b7109689a391fac918eeb18e7d9e76a944369a5
Yesenia <yeseniasescarcega@gmail.com>
2017-03-29 13:52:57 -0600

    Se corrije función crearItems() y crearLista, falta arreglar un detalle en la función borrar(funcionalidad 80%)

--- js/sprint2.js
+++ js/sprint2.js
@@ -1,14 +1,10 @@
 // alert("Bienvenido");
-var contenedor, tarjeta, tituloT, borrarElem,saltoLinea, linea, agregarItems, itemsLista;
+var contenedor, tarjeta, tituloT, borrarElem,saltoLinea, linea, textoItem, agregarItems, itemsLista;
 var cont = 0;//Contador auxiliar para asignar id´s dinámicamente
-
-// function cargarPagina(){
-
-// }
 
 function crearTarjeta(){
   // Se crean los elementos del HTML(<section id = tarjetas>) dinámicamente
-  cont++;
+  cont++;//Aumenta el contador en 1 cada que se crea una tarjeta
   contenedor = document.getElementById("tarjetas");
   tarjeta = document.createElement("span");
   tituloT = document.createElement("h4");
@@ -21,45 +17,46 @@ function crearTarjeta(){
   borrarElem.type = "button";
   borrarElem.value = "Borrar Lista";
   borrarElem.align = "right";
-
+  borrarElem.onclick = borrar;
+  // borrar(tarjeta.id);
   tarjeta.appendChild(borrarElem);
   tarjeta.appendChild(tituloT);
   tarjeta.appendChild(saltoLinea);
   contenedor.appendChild(tarjeta);
-  console.log(tarjeta);
-
+  // console.log(tarjeta);
   document.getElementById("nvaLista").value = "";
-
   crearLista(cont);
 }
 
 function crearLista(cont){
   // Se crean los elementos internos de la variable tarjeta(<span>)
   agregarItems = document.createElement("span");
-  var textoItem = document.createElement("input");
+  textoItem = document.createElement("input");
   var botonItem = document.createElement("input");
   linea = document.createElement("hr");
-
+  // Se asigna valores a los elementos y se anidan al elementoPadre correspondiente
+  textoItem.id = "texto"+cont;
   textoItem.placeholder = "Ingresa tu pendiente";
   botonItem.type = "button";
   botonItem.value = "Agregar Pendiente",
-  // botonItem.onclick = crearItems();
-
+  botonItem.onclick = crearItems;
+  // botonItem.setAttribute("onclick","crearItems();");
   agregarItems.appendChild(textoItem);
   agregarItems.appendChild(botonItem);
   tarjeta.appendChild(agregarItems);
   tarjeta.appendChild(linea);
-
-  crearItems(textoItem.value);
-  // borrarElem.onclick = borrar(tarjeta.id);
+  // textoItem.value ="";
+  // crearItems(); ☒
 
 }
 
 function crearItems(contenido) {
-  console.log(contenido);
-  itemsLista = document.createElement("span");
+  // console.log(contenido);
+  contenido = textoItem.value;
+  var itemsLista = document.createElement("span");
   var lista = document.createElement("ul");
   var nvoItem = document.createElement("li");
+
 
   nvoItem.innerText = contenido;
   lista.appendChild(nvoItem);
@@ -69,8 +66,9 @@ function crearItems(contenido) {
 }
 
 function borrar(id) {
-  //  console.log(id);
+   id = tarjeta.id;
+   console.log(id.value);
   // alert("¿¿¿Estás seguro que quieres borar tu lista???");
-  // var tarjetaAborar = document.getElementById(id);
-  //   contenedor.removeChild(tarjetaAborar);
+  var tarjetaAborrar = document.getElementById(id);
+  contenedor.removeChild(tarjetaAborrar);
 }
